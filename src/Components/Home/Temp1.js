36c00deb2369fac30f7c40132cd74c8e57d993b0
prasanna3511 <80350696+prasanna3511@users.noreply.github.com>
2023-06-24 08:57:56 +0530

    logout added

--- src/Components/Home/Temp1.js
+++ src/Components/Home/Temp1.js
@@ -1,11 +1,27 @@
-import { View, Text } from 'react-native'
+import { View, Text ,Image , Pressable } from 'react-native'
 import React from 'react'
+import profile from '../../Images/profile.png'
+// import {  } from 'react-native-elements'
 
-export default function Temp1() {
+export default function Temp1({navigation}) {
     console.log('this is tab 2')
+    const handlelogout=()=>{
+      console.log('logout')
+      navigation.navigate('login')
+
+    }
   return (
-    <View>
-      <Text>Temp1</Text>
+    <View style={{display:'flex', justifyContent:'space-around', width:'100%' , height:'100%', alignItems:'center'}} >
+      <View style={{width:'100%' , height:'50%' , display:'flex' , justifyContent:'space-around', alignItems:'center'}} >
+
+      <Image source={profile} style={{height:100 ,width:100}}/>
+      <Text style={{color:'black'}} >Temp1</Text>
+      </View>
+
+    <Pressable style={{width:170, height:60, borderRadius:10, display:'flex' , justifyContent:'center' , alignItems:'center' , backgroundColor:'#007993'}} onPress={handlelogout} >
+<Text style={{color:'white' , fontWeight:600 ,fontSize:16}} >Logout</Text>
+    </Pressable>
+
     </View>
   )
 }
